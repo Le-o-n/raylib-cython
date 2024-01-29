13f3ccab643ab761dc5ce6af3df43445e7dec36b
Leon Bass <leonandersonbass@gmail.com>
2024-01-29 23:45:37 +0000

    working sdist and wheel distribution in setup.py

--- shell/build.bat
+++ shell/build.bat
@@ -7,7 +7,7 @@ REM Set the path to the Python interpreter (modify as needed)
 set PYTHON_EXECUTABLE=python
 
 REM Run setup.py for building the package
-%PYTHON_EXECUTABLE% "src\setup.py" build_ext --build-lib=build
+%PYTHON_EXECUTABLE% "setup.py" build_ext --build-lib=build
 
 REM Run setup.py for building the source distribution and compiled distribution
-%PYTHON_EXECUTABLE% "src\setup.py" sdist bdist_wheel
+%PYTHON_EXECUTABLE% "setup.py" sdist bdist_wheel
