cb802ab924cd5839bd17549f8413aa509f3db150
Leon Bass <leonandersonbass@gmail.com>
2024-01-30 20:51:23 +0000

    added script to build in place for testing

---
+++ shell/build_inplace.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal enabledelayedexpansion
+
+@RD /S /Q build 
+@RD /S /Q dist
+@RD /S /Q raylib_cython.egg-info
+
+REM Set the path to the Python interpreter (modify as needed)
+set PYTHON_EXECUTABLE=python
+
+REM Set path to cython
+set CYTHON=cython
+
+REM Run setup.py for building the source distribution and compiled distribution
+%PYTHON_EXECUTABLE% "setup.py" build_ext --inplace
+
+REM Cleanup
+@RD /S /Q "src\raylib_cython.egg-info"
+@DEL /F /Q "src\raylib_cython\raylib.html"
+@DEL /F /Q "src\raylib_cython\raylib.c"
