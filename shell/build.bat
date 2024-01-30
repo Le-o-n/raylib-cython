655c060cc30b3a9eb2cf85befed9b8ba0fabe21a
Leon Bass <leonandersonbass@gmail.com>
2024-01-30 00:47:58 +0000

    updated build

--- shell/build.bat
+++ shell/build.bat
@@ -1,13 +1,15 @@
 @echo off
 setlocal enabledelayedexpansion
 
-@RD /S /Q build dist raylib_cython.egg-info
+@RD /S /Q build 
+@RD /S /Q dist
+@RD /S /Q raylib_cython.egg-info
 
 REM Set the path to the Python interpreter (modify as needed)
 set PYTHON_EXECUTABLE=python
 
-REM Run setup.py for building the package
-%PYTHON_EXECUTABLE% "setup.py" build_ext --build-lib=build
+REM Run setup.py for building the package in place
+%PYTHON_EXECUTABLE% "setup.py" build_ext
 
 REM Run setup.py for building the source distribution and compiled distribution
 %PYTHON_EXECUTABLE% "setup.py" sdist bdist_wheel
