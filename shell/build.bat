216e5da82195be807853a46c9b24cdda253d8886
Leon Bass <leonandersonbass@gmail.com>
2024-01-30 00:59:22 +0000

    updated build to include annotations

--- shell/build.bat
+++ shell/build.bat
@@ -8,8 +8,17 @@ setlocal enabledelayedexpansion
 REM Set the path to the Python interpreter (modify as needed)
 set PYTHON_EXECUTABLE=python
 
+REM Set path to cython
+set CYTHON=cython
+
 REM Run setup.py for building the package in place
 %PYTHON_EXECUTABLE% "setup.py" build_ext
 
+REM Create annotations
+%CYTHON% --annotate -o build/ src/raylib_cython/raylib.pyx
+
 REM Run setup.py for building the source distribution and compiled distribution
 %PYTHON_EXECUTABLE% "setup.py" sdist bdist_wheel
+
+REM Cleanup
+
