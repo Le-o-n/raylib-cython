4f5516b7e42bc4e4055b0ef041fa728be238e649
Leon Bass <leonandersonbass@gmail.com>
2024-02-08 00:51:46 +0000

    ported some functionality over to python

--- main.py
+++ main.py
@@ -3,4 +3,16 @@ import raylib_cython
 from raylib_cython import raylib
 
 
-raylib.main()
+def main():
+    raylib.InitWindow(300, 300, b"Some title")
+
+    while not raylib.WindowShouldClose():
+        raylib.BeginDrawing()
+
+        raylib.EndDrawing()
+
+    raylib.CloseWindow()
+
+
+if __name__ == "__main__":
+    main()
