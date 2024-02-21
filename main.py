85c6fc3158a0c21ab6278ecaaad2434266388c06
Leon Bass <leonandersonbass@gmail.com>
2024-02-21 00:07:45 +0000

    cleaned up residual main file

--- main.py
+++ main.py
@@ -4,15 +4,7 @@ from raylib_cython import raymath
 
 def main():
     
-    x: raymath.CyVector2 = raymath.CyVector2()
-    
-    x.x = 100
-    x.y = 200
-    print(x.x)
-    print(x.y) 
-    x.normalize_inplace()
-    
-    print(x.x)
-    print(x.y)   
+    ...
+       
 if __name__ == "__main__":
     main()
