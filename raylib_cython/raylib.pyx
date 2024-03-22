b2c5c151adf1b921f16f2739912f79e1a268e72e
Leon Bass <leonandersonbass@gmail.com>
2024-03-22 01:16:03 +0000

    updated todo list for enums

--- raylib_cython/raylib.pyx
+++ raylib_cython/raylib.pyx
@@ -2489,3 +2489,264 @@ def get_gesture_pinch_vector() -> raymath.CyVector2:
 def get_gesture_pinch_angle() -> float:
     return GetGesturePinchAngle()
 
+
+
+
+
+
+#TODO ENUMS:
+#ctypedef enum ConfigFlags:
+#        FLAG_VSYNC_HINT         = 0x00000040,   # Set to try enabling V-Sync on GPU
+#        FLAG_FULLSCREEN_MODE    = 0x00000002,   # Set to run program in fullscreen
+#        FLAG_WINDOW_RESIZABLE   = 0x00000004,   # Set to allow resizable window
+#        FLAG_WINDOW_UNDECORATED = 0x00000008,   # Set to disable window decoration (frame and buttons)
+#        FLAG_WINDOW_HIDDEN      = 0x00000080,   # Set to hide window
+#        FLAG_WINDOW_MINIMIZED   = 0x00000200,   # Set to minimize window (iconify)
+#        FLAG_WINDOW_MAXIMIZED   = 0x00000400,   # Set to maximize window (expanded to monitor)
+#        FLAG_WINDOW_UNFOCUSED   = 0x00000800,   # Set to window non focused
+#        FLAG_WINDOW_TOPMOST     = 0x00001000,   # Set to window always on top
+#        FLAG_WINDOW_ALWAYS_RUN  = 0x00000100,   # Set to allow windows running while minimized
+#        FLAG_WINDOW_TRANSPARENT = 0x00000010,   # Set to allow transparent framebuffer
+#        FLAG_WINDOW_HIGHDPI     = 0x00002000,   # Set to support HighDPI
+#        FLAG_WINDOW_MOUSE_PASSTHROUGH = 0x00004000, # Set to support mouse passthrough, only supported when FLAG_WINDOW_UNDECORATED
+#        FLAG_BORDERLESS_WINDOWED_MODE = 0x00008000, # Set to run program in borderless windowed mode
+#        FLAG_MSAA_4X_HINT       = 0x00000020,   # Set to try enabling MSAA 4X
+#        FLAG_INTERLACED_HINT    = 0x00010000    # Set to try enabling interlaced video format (for V3D)
+#    
+#    
+#    # Trace log level
+#    # NOTE: Organized by priority level
+#    ctypedef enum TraceLogLevel:
+#        LOG_ALL = 0,        # Display all logs
+#        LOG_TRACE = 1,          # Trace logging, intended for internal use only
+#        LOG_DEBUG = 2,          # Debug logging, used for internal debugging, it should be disabled on release builds
+#        LOG_INFO = 3,           # Info logging, used for program execution info
+#        LOG_WARNING = 4,        # Warning logging, used on recoverable failures
+#        LOG_ERROR = 5,          # Error logging, used on unrecoverable failures
+#        LOG_FATAL = 6,          # Fatal logging, used to abort program: exit(EXIT_FAILURE)
+#        LOG_NONE = 7            # Disable logging
+#    
+#        
+#    # Keyboard keys (US keyboard layout)
+#    # NOTE: Use GetKeyPressed() to allow redefining
+#    # required keys for alternative layouts
+#    
+#    # Mouse cursor
+#    ctypedef enum MouseCursor:
+#        MOUSE_CURSOR_DEFAULT       = 0,     # Default pointer shape
+#        MOUSE_CURSOR_ARROW         = 1,     # Arrow shape
+#        MOUSE_CURSOR_IBEAM         = 2,     # Text writing cursor shape
+#        MOUSE_CURSOR_CROSSHAIR     = 3,     # Cross shape
+#        MOUSE_CURSOR_POINTING_HAND = 4,     # Pointing hand cursor
+#        MOUSE_CURSOR_RESIZE_EW     = 5,     # Horizontal resize/move arrow shape
+#        MOUSE_CURSOR_RESIZE_NS     = 6,     # Vertical resize/move arrow shape
+#        MOUSE_CURSOR_RESIZE_NWSE   = 7,     # Top-left to bottom-right diagonal resize/move arrow shape
+#        MOUSE_CURSOR_RESIZE_NESW   = 8,     # The top-right to bottom-left diagonal resize/move arrow shape
+#        MOUSE_CURSOR_RESIZE_ALL    = 9,     # The omnidirectional resize/move cursor shape
+#        MOUSE_CURSOR_NOT_ALLOWED   = 10     # The operation-not-allowed shape
+#
+#    # Gamepad buttons
+#    ctypedef enum GamepadButton:
+#        GAMEPAD_BUTTON_UNKNOWN          = 0,    # Unknown button, just for error checking
+#        GAMEPAD_BUTTON_LEFT_FACE_UP     = 1,    # Gamepad left DPAD up button
+#        GAMEPAD_BUTTON_LEFT_FACE_RIGHT  = 2,    # Gamepad left DPAD right button
+#        GAMEPAD_BUTTON_LEFT_FACE_DOWN   = 3,    # Gamepad left DPAD down button
+#        GAMEPAD_BUTTON_LEFT_FACE_LEFT   = 4,    # Gamepad left DPAD left button
+#        GAMEPAD_BUTTON_RIGHT_FACE_UP    = 5,    # Gamepad right button up (i.e. PS3: Triangle, Xbox: Y)
+#        GAMEPAD_BUTTON_RIGHT_FACE_RIGHT = 6,    # Gamepad right button right (i.e. PS3: Square, Xbox: X)
+#        GAMEPAD_BUTTON_RIGHT_FACE_DOWN  = 7,    # Gamepad right button down (i.e. PS3: Cross, Xbox: A)
+#        GAMEPAD_BUTTON_RIGHT_FACE_LEFT  = 8,    # Gamepad right button left (i.e. PS3: Circle, Xbox: B)
+#        GAMEPAD_BUTTON_LEFT_TRIGGER_1   = 9,    # Gamepad top/back trigger left (first), it could be a trailing button
+#        GAMEPAD_BUTTON_LEFT_TRIGGER_2   = 10,   # Gamepad top/back trigger left (second), it could be a trailing button
+#        GAMEPAD_BUTTON_RIGHT_TRIGGER_1  = 11,   # Gamepad top/back trigger right (one), it could be a trailing button
+#        GAMEPAD_BUTTON_RIGHT_TRIGGER_2  = 12,   # Gamepad top/back trigger right (second), it could be a trailing button
+#        GAMEPAD_BUTTON_MIDDLE_LEFT      = 13,   # Gamepad center buttons, left one (i.e. PS3: Select)
+#        GAMEPAD_BUTTON_MIDDLE           = 14,   # Gamepad center buttons, middle one (i.e. PS3: PS, Xbox: XBOX)
+#        GAMEPAD_BUTTON_MIDDLE_RIGHT     = 15,   # Gamepad center buttons, right one (i.e. PS3: Start)
+#        GAMEPAD_BUTTON_LEFT_THUMB       = 16,   # Gamepad joystick pressed button left
+#        GAMEPAD_BUTTON_RIGHT_THUMB      = 17    # Gamepad joystick pressed button right
+#
+#    # Gamepad axis
+#    ctypedef enum GamepadAxis:
+#        GAMEPAD_AXIS_LEFT_X        = 0,     # Gamepad left stick X axis
+#        GAMEPAD_AXIS_LEFT_Y        = 1,     # Gamepad left stick Y axis
+#        GAMEPAD_AXIS_RIGHT_X       = 2,     # Gamepad right stick X axis
+#        GAMEPAD_AXIS_RIGHT_Y       = 3,     # Gamepad right stick Y axis
+#        GAMEPAD_AXIS_LEFT_TRIGGER  = 4,     # Gamepad back trigger left, pressure level: [1..-1]
+#        GAMEPAD_AXIS_RIGHT_TRIGGER = 5      # Gamepad back trigger right, pressure level: [1..-1]
+#
+#    # Material map index
+#    ctypedef enum MaterialMapIndex:
+#        MATERIAL_MAP_ALBEDO         = 0,    # Albedo material (same as: MATERIAL_MAP_DIFFUSE)
+#        MATERIAL_MAP_METALNESS      = 1,    # Metalness material (same as: MATERIAL_MAP_SPECULAR)
+#        MATERIAL_MAP_NORMAL         = 2,    # Normal material
+#        MATERIAL_MAP_ROUGHNESS      = 3,    # Roughness material
+#        MATERIAL_MAP_OCCLUSION      = 4,    # Ambient occlusion material
+#        MATERIAL_MAP_EMISSION       = 5,    # Emission material
+#        MATERIAL_MAP_HEIGHT         = 6,    # Heightmap material
+#        MATERIAL_MAP_CUBEMAP        = 7,    # Cubemap material (NOTE: Uses GL_TEXTURE_CUBE_MAP)
+#        MATERIAL_MAP_IRRADIANCE     = 8,    # Irradiance material (NOTE: Uses GL_TEXTURE_CUBE_MAP)
+#        MATERIAL_MAP_PREFILTER      = 9,    # Prefilter material (NOTE: Uses GL_TEXTURE_CUBE_MAP)
+#        MATERIAL_MAP_BRDF           = 10    # Brdf material
+#
+#    #define MATERIAL_MAP_DIFFUSE      MATERIAL_MAP_ALBEDO
+#    #define MATERIAL_MAP_SPECULAR     MATERIAL_MAP_METALNESS
+#
+#    # Shader location index
+#    ctypedef enum ShaderLocationIndex:
+#        SHADER_LOC_VERTEX_POSITION   = 0,   # Shader location: vertex attribute: position
+#        SHADER_LOC_VERTEX_TEXCOORD01 = 1,   # Shader location: vertex attribute: texcoord01
+#        SHADER_LOC_VERTEX_TEXCOORD02 = 2,   # Shader location: vertex attribute: texcoord02
+#        SHADER_LOC_VERTEX_NORMAL     = 3,   # Shader location: vertex attribute: normal
+#        SHADER_LOC_VERTEX_TANGENT    = 4,   # Shader location: vertex attribute: tangent
+#        SHADER_LOC_VERTEX_COLOR      = 5,   # Shader location: vertex attribute: color
+#        SHADER_LOC_MATRIX_MVP        = 6,   # Shader location: matrix uniform: model-view-projection
+#        SHADER_LOC_MATRIX_VIEW       = 7,   # Shader location: matrix uniform: view (camera transform)
+#        SHADER_LOC_MATRIX_PROJECTION = 8,   # Shader location: matrix uniform: projection
+#        SHADER_LOC_MATRIX_MODEL      = 9,   # Shader location: matrix uniform: model (transform)
+#        SHADER_LOC_MATRIX_NORMAL     = 10,  # Shader location: matrix uniform: normal
+#        SHADER_LOC_VECTOR_VIEW       = 11,  # Shader location: vector uniform: view
+#        SHADER_LOC_COLOR_DIFFUSE     = 12,  # Shader location: vector uniform: diffuse color
+#        SHADER_LOC_COLOR_SPECULAR    = 13,  # Shader location: vector uniform: specular color
+#        SHADER_LOC_COLOR_AMBIENT     = 14,  # Shader location: vector uniform: ambient color
+#        SHADER_LOC_MAP_ALBEDO        = 15,  # Shader location: sampler2d texture: albedo (same as: SHADER_LOC_MAP_DIFFUSE)
+#        SHADER_LOC_MAP_METALNESS     = 16,  # Shader location: sampler2d texture: metalness (same as: SHADER_LOC_MAP_SPECULAR)
+#        SHADER_LOC_MAP_NORMAL        = 17,  # Shader location: sampler2d texture: normal
+#        SHADER_LOC_MAP_ROUGHNESS     = 18,  # Shader location: sampler2d texture: roughness
+#        SHADER_LOC_MAP_OCCLUSION     = 19,  # Shader location: sampler2d texture: occlusion
+#        SHADER_LOC_MAP_EMISSION      = 20,  # Shader location: sampler2d texture: emission
+#        SHADER_LOC_MAP_HEIGHT        = 21,  # Shader location: sampler2d texture: height
+#        SHADER_LOC_MAP_CUBEMAP       = 22,  # Shader location: samplerCube texture: cubemap
+#        SHADER_LOC_MAP_IRRADIANCE    = 23,  # Shader location: samplerCube texture: irradiance
+#        SHADER_LOC_MAP_PREFILTER     = 24,  # Shader location: samplerCube texture: prefilter
+#        SHADER_LOC_MAP_BRDF          = 25   # Shader location: sampler2d texture: brdf
+#
+#    #define SHADER_LOC_MAP_DIFFUSE      SHADER_LOC_MAP_ALBEDO
+#    #define SHADER_LOC_MAP_SPECULAR     SHADER_LOC_MAP_METALNESS
+#
+#    # Shader uniform data type
+#    ctypedef enum ShaderUniformDataType:
+#        SHADER_UNIFORM_FLOAT        = 0,    # Shader uniform type: float
+#        SHADER_UNIFORM_VEC2         = 1,    # Shader uniform type: vec2 (2 float)
+#        SHADER_UNIFORM_VEC3         = 2,    # Shader uniform type: vec3 (3 float)
+#        SHADER_UNIFORM_VEC4         = 3,    # Shader uniform type: vec4 (4 float)
+#        SHADER_UNIFORM_INT          = 4,    # Shader uniform type: int
+#        SHADER_UNIFORM_IVEC2        = 5,    # Shader uniform type: ivec2 (2 int)
+#        SHADER_UNIFORM_IVEC3        = 6,    # Shader uniform type: ivec3 (3 int)
+#        SHADER_UNIFORM_IVEC4        = 7,    # Shader uniform type: ivec4 (4 int)
+#        SHADER_UNIFORM_SAMPLER2D    = 8,    # Shader uniform type: sampler2d
+#
+#    # Shader attribute data types
+#    ctypedef enum ShaderAttributeDataType:
+#        SHADER_ATTRIB_FLOAT = 0,            # Shader attribute type: float
+#        SHADER_ATTRIB_VEC2  = 1,            # Shader attribute type: vec2 (2 float)
+#        SHADER_ATTRIB_VEC3  = 2,            # Shader attribute type: vec3 (3 float)
+#        SHADER_ATTRIB_VEC4  = 3,            # Shader attribute type: vec4 (4 float)
+#
+#    # Pixel formats
+#    # NOTE: Support depends on OpenGL version and platform
+#    ctypedef enum PixelFormat:
+#        PIXELFORMAT_UNCOMPRESSED_GRAYSCALE      = 0,    # 8 bit per pixel (no alpha)
+#        PIXELFORMAT_UNCOMPRESSED_GRAY_ALPHA     = 1,    # 8*2 bpp (2 channels)
+#        PIXELFORMAT_UNCOMPRESSED_R5G6B5         = 2,    # 16 bpp
+#        PIXELFORMAT_UNCOMPRESSED_R8G8B8         = 3,    # 24 bpp
+#        PIXELFORMAT_UNCOMPRESSED_R5G5B5A1       = 4,    # 16 bpp (1 bit alpha)
+#        PIXELFORMAT_UNCOMPRESSED_R4G4B4A4       = 5,    # 16 bpp (4 bit alpha)
+#        PIXELFORMAT_UNCOMPRESSED_R8G8B8A8       = 6,    # 32 bpp
+#        PIXELFORMAT_UNCOMPRESSED_R32            = 7,    # 32 bpp (1 channel - float)
+#        PIXELFORMAT_UNCOMPRESSED_R32G32B32      = 8,    # 32*3 bpp (3 channels - float)
+#        PIXELFORMAT_UNCOMPRESSED_R32G32B32A32   = 9,    # 32*4 bpp (4 channels - float)
+#        PIXELFORMAT_UNCOMPRESSED_R16            = 10,   # 16 bpp (1 channel - half float)
+#        PIXELFORMAT_UNCOMPRESSED_R16G16B16      = 11,   # 16*3 bpp (3 channels - half float)
+#        PIXELFORMAT_UNCOMPRESSED_R16G16B16A16   = 12,   # 16*4 bpp (4 channels - half float)
+#        PIXELFORMAT_COMPRESSED_DXT1_RGB         = 13,   # 4 bpp (no alpha)
+#        PIXELFORMAT_COMPRESSED_DXT1_RGBA        = 14,   # 4 bpp (1 bit alpha)
+#        PIXELFORMAT_COMPRESSED_DXT3_RGBA        = 15,   # 8 bpp
+#        PIXELFORMAT_COMPRESSED_DXT5_RGBA        = 16,   # 8 bpp
+#        PIXELFORMAT_COMPRESSED_ETC1_RGB         = 17,   # 4 bpp
+#        PIXELFORMAT_COMPRESSED_ETC2_RGB         = 18,   # 4 bpp
+#        PIXELFORMAT_COMPRESSED_ETC2_EAC_RGBA    = 19,   # 8 bpp
+#        PIXELFORMAT_COMPRESSED_PVRT_RGB         = 20,   # 4 bpp
+#        PIXELFORMAT_COMPRESSED_PVRT_RGBA        = 21,   # 4 bpp
+#        PIXELFORMAT_COMPRESSED_ASTC_4x4_RGBA    = 22,   # 8 bpp
+#        PIXELFORMAT_COMPRESSED_ASTC_8x8_RGBA    = 23    # 2 bpp
+#
+#    # Texture parameters: filter mode
+#    # NOTE 1: Filtering considers mipmaps if available in the texture
+#    # NOTE 2: Filter is accordingly set for minification and magnification
+#    ctypedef enum TextureFilter:
+#        TEXTURE_FILTER_POINT            = 0,            # No filter, just pixel approximation
+#        TEXTURE_FILTER_BILINEAR         = 1,            # Linear filtering
+#        TEXTURE_FILTER_TRILINEAR        = 2,            # Trilinear filtering (linear with mipmaps)
+#        TEXTURE_FILTER_ANISOTROPIC_4X   = 3,            # Anisotropic filtering 4x
+#        TEXTURE_FILTER_ANISOTROPIC_8X   = 4,            # Anisotropic filtering 8x
+#        TEXTURE_FILTER_ANISOTROPIC_16X  = 5,            # Anisotropic filtering 16x
+#
+#
+#    # Texture parameters: wrap mode
+#    ctypedef enum TextureWrap:
+#        TEXTURE_WRAP_REPEAT             = 0,            # Repeats texture in tiled mode
+#        TEXTURE_WRAP_CLAMP              = 1,            # Clamps texture to edge pixel in tiled mode
+#        TEXTURE_WRAP_MIRROR_REPEAT      = 2,            # Mirrors and repeats the texture in tiled mode
+#        TEXTURE_WRAP_MIRROR_CLAMP       = 3             # Mirrors and clamps to border the texture in tiled mode
+#
+#    # Cubemap layouts
+#    ctypedef enum CubemapLayout:
+#        CUBEMAP_LAYOUT_AUTO_DETECT          = 0,        # Automatically detect layout type
+#        CUBEMAP_LAYOUT_LINE_VERTICAL        = 1,        # Layout is defined by a vertical line with faces
+#        CUBEMAP_LAYOUT_LINE_HORIZONTAL      = 2,        # Layout is defined by a horizontal line with faces
+#        CUBEMAP_LAYOUT_CROSS_THREE_BY_FOUR  = 3,        # Layout is defined by a 3x4 cross with cubemap faces
+#        CUBEMAP_LAYOUT_CROSS_FOUR_BY_THREE  = 4,        # Layout is defined by a 4x3 cross with cubemap faces
+#        CUBEMAP_LAYOUT_PANORAMA             = 5         # Layout is defined by a panorama image (equirrectangular map)
+#
+#    # Font type, defines generation method
+#    ctypedef enum FontType:
+#        FONT_DEFAULT       = 0,         # Default font generation, anti-aliased
+#        FONT_BITMAP        = 1,         # Bitmap font generation, no anti-aliasing
+#        FONT_SDF           = 2          # SDF font generation, requires external shader
+#
+#    # Color blending modes (pre-defined)
+#    ctypedef enum BlendMode:
+#        BLEND_ALPHA             = 0,    # Blend textures considering alpha (default)
+#        BLEND_ADDITIVE          = 1,    # Blend textures adding colors
+#        BLEND_MULTIPLIED        = 2,    # Blend textures multiplying colors
+#        BLEND_ADD_COLORS        = 3,    # Blend textures adding colors (alternative)
+#        BLEND_SUBTRACT_COLORS   = 4,    # Blend textures subtracting colors (alternative)
+#        BLEND_ALPHA_PREMULTIPLY = 5,    # Blend premultiplied textures considering alpha
+#        BLEND_CUSTOM            = 6,    # Blend textures using custom src/dst factors (use rlSetBlendFactors())
+#        BLEND_CUSTOM_SEPARATE   = 7     # Blend textures using custom rgb/alpha separate src/dst factors (use rlSetBlendFactorsSeparate())
+#
+#    # Gesture
+#    # NOTE: Provided as bit-wise flags to enable only desired gestures
+#    ctypedef enum Gesture:
+#        GESTURE_NONE        = 0,        # No gesture
+#        GESTURE_TAP         = 1,        # Tap gesture
+#        GESTURE_DOUBLETAP   = 2,        # Double tap gesture
+#        GESTURE_HOLD        = 4,        # Hold gesture
+#        GESTURE_DRAG        = 8,        # Drag gesture
+#        GESTURE_SWIPE_RIGHT = 16,       # Swipe right gesture
+#        GESTURE_SWIPE_LEFT  = 32,       # Swipe left gesture
+#        GESTURE_SWIPE_UP    = 64,       # Swipe up gesture
+#        GESTURE_SWIPE_DOWN  = 128,      # Swipe down gesture
+#        GESTURE_PINCH_IN    = 256,      # Pinch in gesture
+#        GESTURE_PINCH_OUT   = 512       # Pinch out gesture
+#
+#    # Camera system modes
+#    ctypedef enum CameraMode:
+#        CAMERA_CUSTOM       = 0,        # Custom camera
+#        CAMERA_FREE         = 1,        # Free camera
+#        CAMERA_ORBITAL      = 2,        # Orbital camera
+#        CAMERA_FIRST_PERSON = 3,        # First person camera
+#        CAMERA_THIRD_PERSON = 4         # Third person camera
+#
+#    # Camera projection
+#    ctypedef enum CameraProjection:
+#        CAMERA_PERSPECTIVE  = 0,        # Perspective projection
+#        CAMERA_ORTHOGRAPHIC = 1         # Orthographic projection
+#
+#
+#    # N-patch layout
+#    ctypedef enum NPatchLayout:
+#        NPATCH_NINE_PATCH               = 0,    # Npatch layout: 3x3 tiles
+#        NPATCH_THREE_PATCH_VERTICAL     = 1,    # Npatch layout: 1x3 tiles
+#        NPATCH_THREE_PATCH_HORIZONTAL   = 2     # Npatch layout: 3x1 tiles
